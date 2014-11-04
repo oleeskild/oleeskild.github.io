c30a448687d4719fe2bedc85bae27b7db290f811
Ole Eskild <oleeskilds@gmail.com>
2014-11-04 18:43:43 +0100

    Added movement to box

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-function drawSquare() {
+var square = null;
+
+function move() {
+    if(square === null){
+        return;
+    }
+    var c = document.getElementById("square");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0,0,400,400);
+    draw();
+    
+    square.x+=2;
+    square.y+=2;   
+}
+
+function draw(){
     var c = document.getElementById("square");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0,0,150,75);
+    ctx.fillRect(square.x,square.y,square.w,square.w);
+}
+
+function makeSquare(){
+    
+    square = {
+        x: 0,
+        y: 0,
+        h: 20,
+        w: 20
+    };
 }
 
 
-document.getElementById("square").addEventListener("click", drawSquare);
+
+//window.requestAnimationFrame(move);
+setInterval(move,1000/60);
+document.getElementById("square").addEventListener("click", makeSquare);
